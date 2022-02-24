0c524832972e797181a1f57aaeeb85b1f91bb1b6
shaimaayosef <shimaa.sayed8888@gmail.com>
2022-02-24 23:16:39 +0200

    edit style

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,32 +2,32 @@ import React from 'react'
 
 const Dice = ({diceObject,hold}) => {  
   const styles = {
-                backgroundColor: diceObject.held ? "#dfdb16" : ""
+                backgroundColor: diceObject.held ? "white" : ""
             }   
    
     let dice 
         switch (diceObject.value){
               
             case 1:
-                dice= (<div className="first-face">
+                dice= (<div className="first-face" style={styles} > 
                         <span className="pip"></span>
                 </div>)
                 break;
             case 2:
-                    dice= (<div className="second-face">
+                    dice= (<div className="second-face" style={styles}>
                         <span className="pip"></span>
                         <span className="pip"></span>
                     </div>)
                     break;
             case 3:
-                    dice=  (<div className="third-face">
+                    dice=  (<div className="third-face" style={styles}>
                             <span className="pip"></span>
                             <span className="pip"></span>
                             <span className="pip"></span>
                     </div>)
                     break;
             case 4:
-                    dice= (<div className="fourth-face">
+                    dice= (<div className="fourth-face" style={styles}>
                             <div className="column">
                             <span className="pip"></span>
                             <span className="pip"></span>
@@ -39,7 +39,7 @@ const Dice = ({diceObject,hold}) => {
                         </div>)
                     break;
             case 5:
-                    dice=   (<div className="fifth-face">
+                    dice=   (<div className="fifth-face" style={styles}>
                             <div className="column">
                             <span className="pip"></span>
                             <span className="pip"></span>
@@ -54,7 +54,7 @@ const Dice = ({diceObject,hold}) => {
                         </div>)
                     break;
             case 6:
-                    dice=  (<div className="sixth-face">
+                    dice=  (<div className="sixth-face" style={styles}>
                             <div className="column">
                             <span className="pip"></span>
                             <span className="pip"></span>
@@ -72,7 +72,7 @@ const Dice = ({diceObject,hold}) => {
     }
 
   return (
-    <div  className='held' style={styles} onClick={(e)=> hold (e,diceObject.id)}>
+    <div  className='held'  onClick={(e)=> hold (e,diceObject.id)}>
       {dice}
     </div>
   )
